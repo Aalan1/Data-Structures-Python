169f9004a480627866cf177e0a0f4b77d11960c7
Aalan Mohammad <46545828+Aalan1@users.noreply.github.com>
2021-03-06 21:29:08 -0500

    Add files via upload

---
+++ priorityQueue.py
@@ -0,0 +1,18 @@
+
+
+class Queue():
+    """  My implementation of a Priority Queue using Heaps"""
+    def __init__(self):
+        self.heap = []
+
+    def getParent(index):
+        return floor(i/2)
+
+    def getLeft(index):
+        return 2 * index
+
+    def getRight(index):
+        return (2 * index) + 1
+
+    
+        
